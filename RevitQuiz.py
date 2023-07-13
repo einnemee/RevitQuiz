333896e5ab662b7a14484a6bc5018f205da49d24
Elizabeth Innemee <55023647+einnemee@users.noreply.github.com>
2023-07-13 16:39:53 +0100

    Update RevitQuiz.py
    Question added

--- RevitQuiz.py
+++ RevitQuiz.py
@@ -12,3 +12,9 @@ if answer1 == 'BUILDING INFORMATION MODELLING' or 'BUILDING INFORMATION MODELING
     print ('Correct')
 else: 
     print ('Incorrect')
+
+answer1 = input('Is BIM only available in AutoDesk Revit? Yes/No ').upper()
+if answer1 == 'NO':
+    print ('Correct')
+else: 
+    print ('Incorrect')
