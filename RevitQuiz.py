b8d3970612418ee24be1d0f25d718501b0818064
Elizabeth Innemee <55023647+einnemee@users.noreply.github.com>
2023-07-13 16:32:16 +0100

    Update RevitQuiz.py
    .upper() added
    alternative spelling of modelling added

--- RevitQuiz.py
+++ RevitQuiz.py
@@ -1,14 +1,14 @@
 print ('Revit Quiz Game')
 
-playing = input('Do you want to play? ')
+playing = input('Do you want to play? ').upper()
 
-if playing != 'yes':
+if playing != 'YES':
     quit()
 
 print ('Ok, let\'s play')
 
-answer1 = input('What does BIM stand for? ')
-if answer1 == 'building information modelling':
+answer1 = input('What does BIM stand for? ').upper()
+if answer1 == 'BUILDING INFORMATION MODELLING' or 'BUILDING INFORMATION MODELING':
     print ('Correct')
 else: 
     print ('Incorrect')
